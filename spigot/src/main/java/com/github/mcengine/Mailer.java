a8807d6eb8abf5d88c3b582ce63640fb4252e97c
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-25 19:08:11 +0700

    set up

---
+++ spigot/src/main/java/com/github/mcengine/Mailer.java
@@ -0,0 +1,13 @@
+package com.github.mcengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mailer extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
